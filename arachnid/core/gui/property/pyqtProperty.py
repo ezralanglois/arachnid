c771cecd5c09a273811fe1a7ca35a14f7219a9d1
Robert Langlois <ezralanglois@gmail.com>
2012-09-05 09:39:32 -0400

    Added workaround to document the PyqtProperty class

--- arachnid/core/gui/property/pyqtProperty.py
+++ arachnid/core/gui/property/pyqtProperty.py
@@ -1,5 +1,13 @@
 ''' Extend the pyqtProperty to store more information for the Property editor
 
+This class adds additional attributes to the pyqtProperty class.
+
+
+.. currentmodule:: arachnid.core.gui.property.pyqtProperty
+
+.. autoclass:: PyqtProperty
+    :members:
+
 .. Created on Dec 2, 2010
 .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
 '''
@@ -9,38 +17,39 @@ from PyQt4 import QtCore
 class PyqtProperty(QtCore.pyqtProperty):
     ''' Extend a pyqtProperty to hold additional information
         
-    :Parameters:
-    
-    type : object
-           Type name string or type
-    fget : functor
-           Getter method
-    fset : functor
-           Setter method
-    freset : functor
-           Reset method
-    fdel : functor
-            Delete method
-    doc : str
-          Document string (ignored)
-    designable : bool
-                 Flag property as designable
-    scriptable : bool
-                 Flag property as scriptable
-    stored : bool
-             Flag property as stored
-    user : bool
-             Flag property as User created
-    constant : bool
-               Flag property as constant
-    final : bool
-            Flag property as final
-    editorHints : dict
-                  Editor hints property dictionary
+        :Parameters:
+        
+        type : object
+               Type name string or type
+        fget : functor
+               Getter method
+        fset : functor
+               Setter method
+        freset : functor
+               Reset method
+        fdel : functor
+                Delete method
+        doc : str
+              Document string (ignored)
+        designable : bool
+                     Flag property as designable
+        scriptable : bool
+                     Flag property as scriptable
+        stored : bool
+                 Flag property as stored
+        user : bool
+                 Flag property as User created
+        constant : bool
+                   Flag property as constant
+        final : bool
+                Flag property as final
+        editorHints : dict
+                      Editor hints property dictionary
     '''
     
     def __init__(self, type, fget=None, fset=None, freset=None, fdel=None, doc=None, designable=True, scriptable=True, stored=True, user=False, constant=False, final=False, editorHints={}):
-        "Initialize the property"
+        ''' Create a PyqtProperty object
+        '''
         
         if isinstance(type, str): type = QtCore.QString(type)
         QtCore.pyqtProperty.__init__(self, type, fget, fset, freset, fdel, doc, designable, scriptable, stored, user, constant, final)
