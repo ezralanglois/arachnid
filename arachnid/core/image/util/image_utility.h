8d40f907bf90fe661ea8b7f5d36921dc1e5e3b32
Robert Langlois <ezralanglois@gmail.com>
2012-09-20 08:14:40 -0400

    Fixed segfault - incorrect array index

--- arachnid/core/image/util/image_utility.h
+++ arachnid/core/image/util/image_utility.h
@@ -21,6 +21,7 @@ void rotavg(T* out, int onx, int ony, int onz, T* avg, int na, int rmax)
 				int ir = int(r);
 				if( (rmax-2) <= ir && ir <= rmax )
 				{
+					if( ir >= na ) fprintf(stderr, "error here1\n");
 					padded += avg[ir];
 					padcnt += 1;
 				}
@@ -30,6 +31,9 @@ void rotavg(T* out, int onx, int ony, int onz, T* avg, int na, int rmax)
 	padded /= padcnt;
 
 	int lz=(onz/2+onz%2), ly=(ony/2+ony%2), lx=(onx/2+onx%2);
+	int nz = onx*ony;
+	int tot = nz*onz;
+	if( onz == 1 ) nz = 0;
 #	if defined(_OPENMP_3_0)
 #	pragma omp parallel for collapse(3)
 #	elif defined(_OPENMP)
@@ -43,16 +47,19 @@ void rotavg(T* out, int onx, int ony, int onz, T* avg, int na, int rmax)
 			{
 				T r = std::sqrt( T(z*z) + T(y*y) + T(x*x) );
 				int ir = int(r);
+				int idx = x+onx/2 + (y+ony/2)*onx + (z+onz/2)*nz;
 				if( ir >= rmax )
 				{
-					out[x+y*onx+z*onx*ony] = padded;
+					if( idx >= tot || idx < 0 ) fprintf(stderr, "error here3\n");
+					out[idx] = padded;
 				}
 				else
 				{
-					out[x+y*onx+z*onx*ony] = avg[ir] + ( avg[ir+1] - avg[ir] ) * ( r - T(ir) );
+					if( idx >= tot || idx < 0 ) fprintf(stderr, "error here4\n");
+					if( (ir+1) >= na ) fprintf(stderr, "error here2\n");
+					out[idx] = avg[ir] + ( avg[ir+1] - avg[ir] ) * ( r - T(ir) );
 				}
 			}
 		}
 	}
-
 }
