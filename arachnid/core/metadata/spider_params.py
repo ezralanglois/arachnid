43df0328996e6bada5cf8ff5ab8e609b5694eb10
Robert Langlois <ezralanglois@gmail.com>
2012-09-28 10:54:17 -0400

    Fixed bug - ensure params_file shows up in gui as file and a required_file

--- arachnid/core/metadata/spider_params.py
+++ arachnid/core/metadata/spider_params.py
@@ -202,7 +202,7 @@ def setup_options(parser, pgroup=None, required=False):
     
     from ..app.settings import OptionGroup
     group = OptionGroup(parser, "Params", "Options to control basic SPIDER parameters", id=__name__, gui=dict(root=True, stacked="prepComboBox"))
-    group.add_option("-p", param_file="",  help="Spider parameter file describing a Cryo-EM experiment", required=required)
+    group.add_option("-p", param_file="",  help="Spider parameter file describing a Cryo-EM experiment", required_file=required, gui=dict(filetype="open"))
     group.add_option("-b", bin_factor=1.0, help="Decimatation factor for the script: changes size of images, coordinates, parameters such as pixel_size or window unless otherwise specified")
     if pgroup is not None:
         pgroup.add_option_group(group)
