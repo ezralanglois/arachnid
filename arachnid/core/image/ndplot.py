92e775e61a1409ce70ae7cfb3f35be67b889bbcb
Robert Langlois <ezralanglois@gmail.com>
2012-10-07 18:53:45 -0400

    Fixed bug in backend loading - with sphinx

--- arachnid/core/image/ndplot.py
+++ arachnid/core/image/ndplot.py
@@ -5,20 +5,22 @@
 .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
 '''
 from ..app import tracing
-import logging
+import logging, sys
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 try:
-    import matplotlib
-    try: import pycairo
-    except:matplotlib.use('Agg')
-    else: 
-        try:
-            matplotlib.use('cairo.png')
+    if 'matplotlib' not in set(sys.modules.keys()):
+        import matplotlib
+        try: import pycairo
         except:
-            tracing.log_import_error('Failed to set backend', _logger)
-        pycairo;
+            if matplotlib.get_backend() != "Agg": matplotlib.use('Agg')
+        else: 
+            try:
+                if matplotlib.get_backend() != "cairo.png": matplotlib.use('cairo.png')
+            except:
+                tracing.log_import_error('Failed to set backend', _logger)
+            pycairo;
 except:
     tracing.log_import_error('Failed to set backend', _logger)
     
