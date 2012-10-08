fb51f39656c5f77c5f76707bb0ae81cb15018e3e
Robert Langlois <ezralanglois@gmail.com>
2012-10-08 14:37:54 -0400

    Added function - convert Euler angles to coarser grid

--- arachnid/core/orient/healpix.py
+++ arachnid/core/orient/healpix.py
@@ -73,4 +73,50 @@ def ang2pix(resolution, theta, phi=None, scheme='ring', half=False, out=None):
         if half and theta >= 180.0: theta = theta - 180.0
         return _ang2pix(int(resolution), float(theta), float(phi))
 
+def coarse(resolution, theta, phi=None, scheme='ring', half=False, out=None):
+    ''' Convert Euler angles to coarser grid
+    
+    :Parameters:
+    
+    resolution : int
+                 Pixel resolution
+    theta : float or array
+            Euler angle theta or array of Euler angles
+    phi : float
+          Euler angle phi or optional if theta is array of both
+    scheme : str
+             Pixel layout scheme: nest or ring
+    half : bool
+           Convert Euler angles to half volume
+    out : array, optional
+          Array of pixels for specified array of Euler angles
+    
+    :Returns:
+    
+    out : in or, array
+          Pixel for specified Euler angles
+    '''
+    import healpix
+    
+    if scheme not in ('nest', 'ring'): raise ValueError, "scheme must be nest or ring"
+    if hasattr(theta, '__iter__'):
+        if phi is not None and not hasattr(phi, '__iter__'): 
+            raise ValueError, "phi must be None or array when theta is an array"
+        if hasattr(phi, '__iter__'): theta = zip(theta, phi)
+        _ang2pix = getattr(healpix, 'ang2pix_%s'%scheme)
+        _pix2ang = getattr(healpix, 'pix2ang_%s'%scheme)
+        if out is None: out = numpy.zeros((len(theta), 2))
+        i = 0
+        for t, p in theta:
+            if half and t >= 180.0: t = t - 180.0
+            pix = _ang2pix(int(resolution), float(t), float(p))
+            out[i, :] = _pix2ang(int(resolution), int(pix))
+            i += 1
+        return out
+    else:
+        _ang2pix = getattr(healpix, 'ang2pix_%s'%scheme)
+        if phi is None: "phi must not be None when theta is a float"
+        if half and theta >= 180.0: theta = theta - 180.0
+        return _ang2pix(int(resolution), float(theta), float(phi))
+
 
