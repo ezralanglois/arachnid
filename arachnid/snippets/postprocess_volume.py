e429fed79c8091b168a54b54c2b642b0f52e2fae
Robert Langlois <ezralanglois@gmail.com>
2012-10-08 11:57:13 -0400

    Updated documentation to reflect name change

--- arachnid/snippets/postprocess_volume.py
+++ arachnid/snippets/postprocess_volume.py
@@ -1,14 +1,14 @@
 ''' Post-process a raw volume for refinement
 
-Download to edit and run: :download:`prepare_volume.py <../../arachnid/snippets/prepare_volume.py>`
+Download to edit and run: :download:`postprocess_volume.py <../../arachnid/snippets/postprocess_volume.py>`
 
 To run:
 
 .. sourcecode:: sh
     
-    $ python prepare_volume.py
+    $ python postprocess_volume.py
 
-.. literalinclude:: ../../arachnid/snippets/prepare_volume.py
+.. literalinclude:: ../../arachnid/snippets/postprocess_volume.py
    :language: python
    :lines: 16-
    :linenos:
@@ -22,19 +22,23 @@ if __name__ == '__main__':
     
     input_volume = "raw.spi"
     output_volume = "vol"
+    
     params_file="params"
-    resolution = 12.0 # Set Zero to disable
-    pixel_size = 1.2 # If there is a params file, do not set this
-    hi_resolution=0.0 # Set Zero to disable
+    pixel_size = 1.2        # If there is a params file, do not set this
+    pixel_diameter = 200    # If there is a params file, do not set this
+    
+    # Filter parameters
+    resolution = 12.0   # Set Zero to disable
+    hi_resolution=0.0   # Set Zero to disable
     bw_pass = 0.05
     bw_stop = 0.05
     hp_bw_pass=bw_pass
     hp_bw_stop=bw_stop
     
-    mask_type = "tight"
+    # Mask parameters
+    mask_type = "none" # Set to tight for tight masking and spherical for spherical masking
     
     # Tight mask parameters
-    
     threshold_for_binary_mask=None # None means find threshold automatically
     number_of_times_to_dialate_binary_mask=1
     size_of_the_gaussian_kernel=3
@@ -43,7 +47,6 @@ if __name__ == '__main__':
     # Spherical Mask parameters
     mask_edge_width = 3
     mask_edge_type = 'C'
-    pixel_diameter = 200 # If there is a params file, do not set this
     
     # Invoke a SPIDER session using the extension from input_volume
     spi = spider.open_session([input_volume], spider_path="", thread_count=0, enable_results=False) # Create a SPIDER session using the extension of the input_volume
@@ -75,6 +78,7 @@ if __name__ == '__main__':
     
     spi.cp(involume, outputfile=output_volume)
     
+    # Mask the volume
     if mask_type == "tight":
         img = ndimage_file.read_image(output_volume)                                                     # Read volume from a file
         mask, th = ndimage_utility.tight_mask(img, threshold=threshold_for_binary_mask, ndilate=number_of_times_to_dialate_binary_mask, gk_size=size_of_the_gaussian_kernel, gk_sigma=standard_deviation_of_gaussian)    # Generate a adaptive tight mask
@@ -84,3 +88,6 @@ if __name__ == '__main__':
         center = spider.image_size(spi, input_volume)[0]/2+1                                                                   # Center of the volume
         radius = pixel_diameter/2+mask_edge_width/2 if mask_edge_type == 'C' else pixel_diameter/2+mask_edge_width                # Define radius of the mask
         spi.ma(involume, radius, (center, center, center), mask_edge_type, 'C', mask_edge_width, outputfile=output_volume) # Create a spherical mask with a soft edge
+
+
+
