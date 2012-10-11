9d659040a115e8e408074b4f924765e380d8fde3
Robert Langlois <ezralanglois@gmail.com>
2012-10-11 13:28:16 -0400

    Fixed bug - renamed param to extra

--- arachnid/pyspider/autorefine.py
+++ arachnid/pyspider/autorefine.py
@@ -190,7 +190,7 @@ def refine_volume(spi, alignvals, curr_slice, refine_index, output, resolution_s
         bin_factor = decimation_level(resolution_start, **extra)
         extra.update(spider_params.update_params(bin_factor, **param))
         extra['trans_range'] = ensure_translation_range(**extra)
-        param['ring_last'] = int(param['pixel_diameter']/2.0)
+        extra['ring_last'] = int(param['pixel_diameter']/2.0)
         extra['theta_delta'] = theta_delta_est(resolution_start, **extra)
         shuffle_angles = extra['theta_delta'] == theta_prev and refine_index > 0
         extra['min_resolution'] = filter_resolution(resolution_start, **extra)
