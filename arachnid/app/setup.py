e6116ebf060c6f797f247e55ca5d7bba8e4f6d35
Robert Langlois <ezralanglois@gmail.com>
2012-10-19 13:37:12 -0400

    Added autoclean and defocus to setup.py

--- arachnid/app/setup.py
+++ arachnid/app/setup.py
@@ -4,6 +4,6 @@
 console_scripts = [
  'autopick = arachnid.app.autopick:main',
  'lfcpick = arachnid.app.lfcpick:main',
- #'defocus = arachnid.app.defocus:main',
- #'autoclean = arachnid.app.autoclean:main',
+ 'defocus = arachnid.app.defocus:main',
+ 'autoclean = arachnid.app.autoclean:main',
 ]
