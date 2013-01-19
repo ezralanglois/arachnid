5a63c5296b2d04fac57887882bb2ed043ce0056e
Robert Langlois <ezralanglois@gmail.com>
2013-01-19 09:48:49 -0500

    Added autoctf algorithm

--- arachnid/app/setup.py
+++ arachnid/app/setup.py
@@ -7,5 +7,6 @@ console_scripts = [
  'defocus = arachnid.app.defocus:main',
  'autoclean = arachnid.app.autoclean:main',
  'ctf-select = arachnid.app.ctf_select:main',
+ 'autoctf = arachnid.app.autoctf:main',
  'classify3d = arachnid.app.classify3d:main',
 ]
