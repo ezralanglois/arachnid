8076ecb467568dfff6ced20f72deb9b58782b6fd
Robert Langlois <ezralanglois@gmail.com>
2013-01-22 11:09:17 -0500

    Added comment to update compiler flags

--- arachnid/core/image/spi/setup.py
+++ arachnid/core/image/spi/setup.py
@@ -20,12 +20,14 @@ def configuration(parent_package='',top_path=None):
     if 1 == 0:
         f2py_options = ['--debug-capi']
     else: f2py_options=[]
+    
+    #-ffixed-form
     compiler_options=dict(define_macros=[('SP_LIBFFTW3', 1)], macros=[('SP_LIBFFTW3', 1)])#extra_f77_compiler_args=['-fdefault-real-8'],, ('SP_MP', 1)
                           #extra_f90_compiler_args=['-fdefault-real-8'])
     config.add_library('spiutil', sources=['spiutil.F90', 'spider/fftw3.F90', 'spider/fmrs.F90', 'spider/fmrs_2.F90', 'spider/besi1.F90'], depends=['spider/CMBLOCK.INC', 'spider/FFTW3.INC'], **compiler_options) #, 'fmrs_info.mod', 'type_kinds.mod'
     fftlibs = fftw_opt['libraries']
     del fftw_opt['libraries']
-    config.add_extension('_spider_reconstruct', sources=['backproject_nn4.f90', 'backproject_bp3f.f90'], libraries=['spiutil']+fftlibs, f2py_options=f2py_options, **fftw_opt)#, **compiler_options)
+    config.add_extension('_spider_reconstruct', sources=['backproject_nn4.f90', 'backproject_bp3f.f90'], libraries=['spiutil']+fftlibs, f2py_options=f2py_options)#, **fftw_opt)#, **compiler_options)
     config.add_include_dirs(os.path.dirname(__file__))
     config.add_include_dirs(os.path.join(os.path.dirname(__file__), 'spider'))
     return config
