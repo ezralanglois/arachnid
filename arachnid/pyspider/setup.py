f5c75e80b5f3e9544686a11c936cc4548786b154
Robert Langlois <ezralanglois@gmail.com>
2013-04-23 16:11:16 -0400

    Added three new pyspider apps: prepbootstrap and old refine - saferefine

--- arachnid/pyspider/setup.py
+++ arachnid/pyspider/setup.py
@@ -16,5 +16,9 @@ console_scripts = [
  'refine = arachnid.pyspider.refine:main',
  'resolution = arachnid.pyspider.resolution:main',
  'autorefine = arachnid.pyspider.autorefine:main',
+ 'rename2spi = arachnid.pyspider.legion_to_spider:main',
+ 'prepbootstrap = arachnid.pyspider.prepare_bootstrap:main',
+ 'pwmovie = arachnid.pyspider.autoctf_movie:main',
+ 'saferefine = arachnid.pyspider.saferefine:main',
 ]
 
