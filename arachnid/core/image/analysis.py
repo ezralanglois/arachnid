2d4e7e7b83fb981a8c6fa3ae906c26673db9ae23
Robert Langlois <ezralanglois@gmail.com>
2013-05-21 13:10:55 -0400

    Added fast pca

--- arachnid/core/image/analysis.py
+++ arachnid/core/image/analysis.py
@@ -13,6 +13,7 @@ import numpy, scipy.special, scipy.linalg
 from ..parallel import process_queue
 from ..util import numpy_ext
 import logging, functools
+import manifold
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
@@ -278,6 +279,126 @@ def pca_train(trn, frac=-1, mtrn=None):
     #_logger.error("pca: %s -- %s"%(str(V.shape), str(idx)))
     if idx >= len(d): idx = 1
     return idx, V[:idx], numpy.sum(t[:idx])
+
+def pca_fast(trn, tst=None, frac=0.0, centered=False):
+    '''
+    '''
+    
+    if not centered: trn -= trn.mean(0)
+    _logger.error("here1")
+    if trn.shape[1] <= trn.shape[0]:
+        _logger.error("here2")
+        #C = numpy.cov(trn)
+        C = numpy.dot(trn.transpose(), trn)
+    else:
+        _logger.error("here3: %s"%str(trn.shape))
+        C = manifold.fastdot_t2(trn, trn, None, 1.0/trn.shape[0])
+        #C = numpy.dot(trn, trn.T)
+        #numpy.multiply(C, 1.0/trn.shape[0], C)
+    _logger.error("here4")
+    d, V = numpy.linalg.eigh(C)
+    _logger.error("here5")
+    
+    if trn.shape[1] > trn.shape[0]:
+        _logger.error("here6")
+        #V = numpy.dot(trn.T, V)
+        V = numpy.ascontiguousarray(V)
+        V = manifold.fastdot_t1(trn, V)
+        
+        err = numpy.seterr(divide='ignore', invalid='ignore')
+        d = 1.0/numpy.sqrt(d)
+        numpy.seterr(**err)
+        d = numpy.where(numpy.isfinite(d), d, 0)
+        V *= d
+    d = numpy.where(d < 0, 0, d)
+    d /= d.sum()
+    idx = d.argsort()[::-1]
+    d = d[idx]
+    V = V[:, idx]
+    
+    if tst is not None:
+        if frac >= 1:
+            idx = int(frac)
+        elif frac == 0.0:
+            idx = d.shape[0]
+        else:
+            idx = numpy.sum(d.cumsum()<frac)+1
+        val = manifold.fastdot_t2(V[:, :idx].transpose().copy(), tst).T
+        #val = numpy.dot(V[:, :idx].T, tst.T).T
+        return V, d, val
+    return V, d
+
+'''
+import numpy
+
+def pca_svd(flat):
+   u, s, vt = numpy.linalg.svd(flat, full_matrices = 0)
+   pcs = vt
+   v = numpy.transpose(vt)
+   data_count = len(flat)
+   variances = s**2 / data_count
+   positions =  u * s
+   return pcs, variances, positions
+
+def pca_eig(flat):
+   values, vectors = _symm_eig(flat)
+   pcs = vectors.transpose()
+   variances = values / len(flat)
+   positions = numpy.dot(flat, vectors)
+   return pcs, variances, positions
+
+def _symm_eig(a):
+   """Given input a, return the non-zero eigenvectors and eigenvalues  
+of the symmetric matrix a'a.
+
+   If a has more columns than rows, then that matrix will be rank- 
+deficient,
+   and the non-zero eigenvalues and eigenvectors of a'a can be more  
+easily extracted
+   from the matrix aa'. From the properties of the SVD:
+     if a of shape (m,n) has SVD u*s*v', then:
+       a'a = v*s's*v'
+       aa' = u*ss'*u'
+     let s_hat, an array of shape (m,n), be such that s * s_hat = I(m,m)
+     and s_hat * s = I(n,n). Thus, we can solve for u or v in terms of  
+the other:
+       v = a'*u*s_hat'
+       u = a*v*s_hat
+   """
+   m, n = a.shape
+   if m >= n:
+     # just return the eigenvalues and eigenvectors of a'a
+     vecs, vals = _eigh(numpy.dot(a.transpose(), a))
+     vecs = numpy.where(vecs < 0, 0, vecs)
+     return vecs, vals
+   else:
+     # figure out the eigenvalues and vectors based on aa', which is  
+smaller
+     sst_diag, u = _eigh(numpy.dot(a, a.transpose()))
+     # in case due to numerical instabilities we have sst_diag < 0  
+anywhere,
+     # peg them to zero
+     sst_diag = numpy.where(sst_diag < 0, 0, sst_diag)
+     # now get the inverse square root of the diagonal, which will  
+form the
+     # main diagonal of s_hat
+     err = numpy.seterr(divide='ignore', invalid='ignore')
+     s_hat_diag = 1/numpy.sqrt(sst_diag)
+     numpy.seterr(**err)
+     s_hat_diag = numpy.where(numpy.isfinite(s_hat_diag), s_hat_diag, 0)
+     # s_hat_diag is a list of length m, a'u is (n,m), so we can just  
+use
+     # numpy's broadcasting instead of matrix multiplication, and only  
+create
+     # the upper mxm block of a'u, since that's all we'll use anyway...
+     v = numpy.dot(a.transpose(), u[:,:m]) * s_hat_diag
+     return sst_diag, v
+
+def _eigh(m):
+   values, vectors = numpy.linalg.eigh(m)
+   order = numpy.flipud(values.argsort())
+   return values[order], vectors[:,order]
+'''
 
 def pca(trn, tst=None, frac=-1, mtrn=None, use_svd=True):
     ''' Principal component analysis using SVD
