798bbd812bc9145e9783030f1b21455d4a7a6920
Robert Langlois <ezralanglois@gmail.com>
2013-07-05 11:32:24 -0400

    Added support openmp compile, removed _spider_util

--- arachnid/core/image/util/setup.py
+++ arachnid/core/image/util/setup.py
@@ -9,6 +9,7 @@ This setup file defines a build script for C/C++ or Fortran extensions.
 def configuration(parent_package='',top_path=None):  
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info
+    from arachnid.setup import compiler_options
     import os
     
     try:
@@ -17,16 +18,12 @@ def configuration(parent_package='',top_path=None):
         blas_opt = get_info('blas')
     
     config = Configuration('util', parent_package, top_path)
-    if 1 == 1:
-        config.add_library('spider_utillib', sources=['spider_lib.f90'])#,  define_macros=[('SP_LIBFFTW3', 1)])
-    else:
-        config.add_library('spider_util', sources=['spider_lib.f90', 'spider/fmrs_2.f', 'spider/fmrs.f'],  define_macros=[('SP_LIBFFTW3', 1)], 
-                           extra_f90_compile_args=['-x f95-cpp-input', '-fopenmp', '-D__OPENMP'],#-cpp -DSP_GFORTRAN -DSP_LIBFFTW3 -DSP_LINUX -O3 -funroll-loops -finline-limit=600 -DSP_MP -fopenmp
-                           extra_f77_compile_args=['-x f95-cpp-input', '-fopenmp', '-D__OPENMP'])
-
-    config.add_extension('_spider_util', sources=['spider_util.f90'], libraries=['spider_utillib'])
-    config.add_extension('_image_utility', sources=['image_utility.i', 'radon.c'], define_macros=[('__STDC_FORMAT_MACROS', 1)], depends=['image_utility.h'], swig_opts=['-c++'])
-    config.add_extension('_manifold', sources=['manifold.i'], define_macros=[('__STDC_FORMAT_MACROS', 1)], depends=['manifold.hpp'], swig_opts=['-c++'], extra_info = blas_opt)
+    
+    
+    compiler_args, compiler_libraries, compiler_defs = compiler_options()[3:]
+    
+    config.add_extension('_image_utility', sources=['image_utility.i', 'radon.c'], define_macros=[('__STDC_FORMAT_MACROS', 1)]+compiler_defs, depends=['image_utility.h'], swig_opts=['-c++'], extra_compile_args=compiler_args, extra_link_args=compiler_args, libraries=compiler_libraries)
+    config.add_extension('_manifold', sources=['manifold.i'], define_macros=[('__STDC_FORMAT_MACROS', 1)]+compiler_defs, depends=['manifold.hpp'], swig_opts=['-c++'], extra_info = blas_opt, extra_compile_args=compiler_args, extra_link_args=compiler_args, libraries=compiler_libraries)
     config.add_include_dirs(os.path.dirname(__file__))
     return config
 
