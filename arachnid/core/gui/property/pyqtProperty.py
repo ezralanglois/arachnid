449867c2df079c827dc8b20910a9e5d492263c20
Robert Langlois <ezralanglois@gmail.com>
2013-07-08 11:10:02 -0400

    Created PySide version of PyQtProperty - still does not work, needs rewrite

--- arachnid/core/gui/property/pyqtProperty.py
+++ arachnid/core/gui/property/pyqtProperty.py
@@ -12,49 +12,64 @@ This class adds additional attributes to the pyqtProperty class.
 .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
 '''
 
-from ..util.qt4_loader import QtCore, qtProperty
+from ..util.qt4_loader import qtProperty
 
-class PyqtProperty(qtProperty):
-    ''' Extend a pyqtProperty to hold additional information
-        
-        :Parameters:
-        
-        type : object
-               Type name string or type
-        fget : functor
-               Getter method
-        fset : functor
-               Setter method
-        freset : functor
-               Reset method
-        fdel : functor
-                Delete method
-        doc : str
-              Document string (ignored)
-        designable : bool
-                     Flag property as designable
-        scriptable : bool
-                     Flag property as scriptable
-        stored : bool
-                 Flag property as stored
-        user : bool
-                 Flag property as User created
-        constant : bool
-                   Flag property as constant
-        final : bool
-                Flag property as final
-        editorHints : dict
-                      Editor hints property dictionary
-    '''
-    
-    def __init__(self, index, type, fget=None, fset=None, freset=None, fdel=None, doc=None, designable=True, scriptable=True, stored=True, user=False, constant=False, final=False, editorHints={}):
-        ''' Create a PyqtProperty object
+try:
+    class PyqtProperty(qtProperty):
+        ''' Extend a pyqtProperty to hold additional information
+            
+            :Parameters:
+            
+            type : object
+                   Type name string or type
+            fget : functor
+                   Getter method
+            fset : functor
+                   Setter method
+            freset : functor
+                   Reset method
+            fdel : functor
+                    Delete method
+            doc : str
+                  Document string (ignored)
+            designable : bool
+                         Flag property as designable
+            scriptable : bool
+                         Flag property as scriptable
+            stored : bool
+                     Flag property as stored
+            user : bool
+                     Flag property as User created
+            constant : bool
+                       Flag property as constant
+            final : bool
+                    Flag property as final
+            editorHints : dict
+                          Editor hints property dictionary
         '''
         
-        QtCore.pyqtProperty.__init__(self, type, fget, fset, freset, fdel, doc, designable, scriptable, stored, user, constant, final)
-        self.editorHints = editorHints
-        self.doc = doc
-        self.order_index = index
+        
+        def __init__(self, index, type, fget=None, fset=None, freset=None, fdel=None, doc=None, designable=True, scriptable=True, stored=True, user=False, constant=False, final=False, editorHints={}):
+            ''' Create a PyqtProperty object
+            '''
+            
+            qtProperty.__init__(self, type, fget, fset, freset, fdel, doc, designable, scriptable, stored, user, constant, final)
+            self.editorHints = editorHints
+            self.doc = doc
+            self.order_index = index
+except:
+    raise # TODO: Need to find a hack for PySide
+    def PyqtProperty(index, type, fget=None, fset=None, freset=None, fdel=None, doc=None, notify=None, designable=True, scriptable=True, stored=True, user=False, constant=False, final=False, editorHints={}):
+        '''
+        '''
+        
+        prop=qtProperty(type, fget, fset, freset, fdel, doc, notify, designable, scriptable, stored, user, constant, final)
+        #setattr(prop, 'editorHints', editorHints)
+        #prop.editorHints=editorHints
+        return prop
+    
+    
+    
     
 
 
