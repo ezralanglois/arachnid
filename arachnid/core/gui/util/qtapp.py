b329b7ae89d6c42b193884e9f40c3d68a7e2b51d
Robert Langlois <ezralanglois@gmail.com>
2013-07-27 07:58:57 -0400

    Renamed organization

--- arachnid/core/gui/util/qtapp.py
+++ arachnid/core/gui/util/qtapp.py
@@ -22,7 +22,7 @@ def create_app():
     import arachnid
     if QtGui is None: return None
     app = QtGui.QApplication([])
-    QtCore.QCoreApplication.setOrganizationName("Frank Lab")
+    QtCore.QCoreApplication.setOrganizationName("CryoSoft")
     QtCore.QCoreApplication.setOrganizationDomain(arachnid.__url__)
     QtCore.QCoreApplication.setApplicationName(arachnid.__project__)
     QtCore.QCoreApplication.setApplicationVersion(arachnid.__version__)
