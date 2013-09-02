a8e92450f537ed62f1de56ab5404af8a6b951944
Robert Langlois <ezralanglois@gmail.com>
2013-09-02 08:29:38 -0400

    Fixed tuple2numpy for relion

--- arachnid/core/metadata/format_utility.py
+++ arachnid/core/metadata/format_utility.py
@@ -71,7 +71,7 @@ def combine(vals):
 try:
     import numpy
 
-    def tuple2numpy(blobs, out=None):
+    def tuple2numpy(blobs, out=None, convert=None):
         ''' Convert a list of namedtuples to a numpy array
         
         :Parameters:
@@ -91,16 +91,46 @@ try:
         
         if len(blobs) == 0: return []
         header = list(blobs[0]._fields)
+        idx=None
         if has_file_id(blobs):
             header.insert(0, "fileid")
             if out is None: out = numpy.zeros((len(blobs), len(blobs[0])+1))
-            for i in xrange(len(blobs)):
-                out[i, 0] = file_id(blobs[i].id)
-                out[i, 1:] = numpy.asarray(blobs[i]._replace(id=object_id(blobs[i].id)))
+            if hasattr(blobs[0], 'id'):
+                for i in xrange(len(blobs)):
+                    out[i, 0] = file_id(blobs[i].id)
+                    out[i, 1:] = numpy.asarray(blobs[i]._replace(id=object_id(blobs[i].id)))
+            elif hasattr(blobs[0], 'rlnImageName'):
+                for i in xrange(len(blobs)):
+                    if convert is not None:
+                        fid, pid = convert(blobs[i].rlnImageName)
+                        tmp = numpy.asarray(blobs[i]._replace(rlnImageName=pid))
+                    else:
+                        fid, tmp = file_id(blobs[i].rlnImageName), numpy.asarray(blobs[i]._replace(id=object_id(blobs[i].rlnImageName)))
+                    out[i, 0] = fid
+                    if idx is None:
+                        try:
+                            out[i, 1:] = tmp
+                        except:
+                            tmp = blobs[i]._replace(rlnImageName=pid)
+                            idx = []
+                            for i in xrange(len(tmp)):
+                                try: float(tmp[i])
+                                except: pass
+                                else: idx.append(i)
+                    if idx is not None:
+                        tmp = blobs[i]._replace(rlnImageName=pid)
+                        out[i, 1:1+len(idx)] = numpy.asarray([float(tmp[j]) for j in idx])
+                        
+            else: raise ValueError, "Cannot find ID column"
         else:
             if out is None: out = numpy.zeros((len(blobs), len(blobs[0])))
             for i in xrange(len(blobs)):
                 out[i, :] = numpy.asarray(blobs[i])
+        if idx is not None:
+            headerold=list(header)
+            header=[]
+            for i in idx:
+                header.append(headerold[i])
         return out, header
     
     def create_selection_doc(n, start=1, micrograph_id=None):
@@ -600,7 +630,9 @@ def has_file_id(val):
     if isinstance(val, tuple):
         try:
             return has_file_id(val.id)
-        except: return False
+        except: 
+            try:return has_file_id(val.rlnImageName)
+            except: return False
     elif isinstance(val, list):
         return has_file_id(val[0])
     else:
