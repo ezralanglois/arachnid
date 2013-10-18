89ad94db4b6f6c4546ab12b1850f28425b836654
Robert Langlois <ezralanglois@gmail.com>
2013-10-18 19:17:54 -0400

    Removed eman2 reconstruction engine

--- arachnid/core/image/reconstruct.py
+++ arachnid/core/image/reconstruct.py
@@ -10,23 +10,18 @@ Supported Reconstruction Methods
 .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
 '''
 from ..app import tracing
-from ..parallel import mpi_utility, process_tasks #, process_queue
-#from ..orient import transforms
-#import ndimage_utility
-try: import eman2_utility
-except: raise
-import logging, numpy #, scipy.fftpack
+from ..parallel import mpi_utility, process_tasks
+import logging, numpy
 
 _logger = logging.getLogger(__name__)
-_logger.setLevel(logging.DEBUG) #img = eman2_utility.ramp(img)
+_logger.setLevel(logging.DEBUG)
 
 try: 
     from spi import _spider_reconstruct
     _spider_reconstruct;
 except:
     _spider_reconstruct=None
-    _logger.exception("Module failed to load")
-    tracing.log_import_error('Failed to load _spider_util.so module - certain functions will not be available: ndimage_utility.ramp', _logger)
+    tracing.log_import_error('Failed to load _spider_reconstruct.so', _logger)
 
 def reconstruct3_bp3f_mp(image_size, gen1, gen2, align1=None, align2=None, **extra):
     '''Reconstruct three volumes using BP3F
@@ -208,62 +203,6 @@ def backproject_bp3n(gen, image_size, align, process_number, npad=2, **extra):
         raise
     return forvol, weight
 
-def reconstruct_nn4_mp(gen, image_size, align, **extra):
-    '''Reconstruct a single volume with the given image generator and alignment
-    file.
-    
-    :Parameters:
-    
-    gen : array generator
-          Generate a sequence of images in the array format
-    align : str
-            Input alignment file
-    extra : dict
-            Unused keyword arguments
-    
-    :Returns:
-        
-    vol : array
-          Reconstruction volume (IF MPI, then only to the root, otherwise None)
-    '''
-    
-    recon, fftvol, weight, image_size = eman2_utility.setup_nn4(image_size, **extra)
-    for v, w in process_tasks.iterate_reduce(gen, eman2_utility.backproject_nn4_new, align=align, **extra):
-        fftvol += v
-        weight += w
-    return eman2_utility.finalize_nn4(recon)
-
-def reconstruct_nn4(gen, align=None, npad=2, sym='c1', weighting=1, **extra):
-    '''Reconstruct a single volume with the given image generator and alignment
-    file.
-    
-    :Parameters:
-    
-    gen : array generator
-          Generate a sequence of images in the array format
-    align : str
-            Input alignment file
-    npad : int
-           Number of times to pad image before FFT
-    sym : str
-          Type of symmetry in the volume
-    weighting : float
-                Weight for the projections
-    extra : dict
-            Unused keyword arguments
-    
-    :Returns:
-        
-    vol : array
-          Reconstruction volume (IF MPI, then only to the root, otherwise None)
-    '''
-    
-    recon = eman2_utility.backproject_nn4(gen, align, npad=npad, sym=sym, weighting=weighting)
-    mpi_utility.block_reduce(recon[1].ravel(), **extra)
-    mpi_utility.block_reduce(recon[2].ravel(), **extra)
-    if mpi_utility.is_root(**extra):
-        return eman2_utility.finalize_nn4(recon)
-
 def reconstruct3_mp(backproject, finalize, image_size, gen1, gen2, align1=None, align2=None, npad=2, cleanup_fft=True, **extra):
     '''Reconstruct three volumes using BP3F
     
@@ -336,67 +275,6 @@ def reconstruct3_mp(backproject, finalize, image_size, gen1, gen2, align1=None, 
         else:
             finalize(None, None, 0, cleanup_fft)
         return None
-
-def reconstruct_nn4_3(gen1, gen2, align1=None, align2=None, npad=2, sym='c1', weighting=1, **extra):
-    '''Reconstruct a full volume and two half volumes with the given image generator and alignment
-    file.
-    
-    :Parameters:
-    
-    gen1 : array generator
-          Generate a sequence of images in the array format (for first half volume)
-    gen2 : array generator
-          Generate a sequence of images in the array format (for second half volume)
-    align1 : str
-            Input alignment file for first half volume
-    align2 : str
-            Input alignment file for second half volume
-    npad : int
-           Number of times to pad image before FFT
-    sym : str
-          Type of symmetry in the volume
-    weighting : float
-                Weight for the projections
-    extra : dict
-            Unused keyword arguments
-    
-    :Returns:
-        
-    vol : array
-          Reconstruction full volume (IF MPI, then only to the root, otherwise None)
-    vol1 : array
-          Reconstruction half volume (IF MPI and node rank == 1, otherwise None)
-    vol2 : array
-          Reconstruction half volume (IF MPI and node rank == 2, otherwise None)
-    '''
-    
-    root = 1 if mpi_utility.get_size(**extra) > 2 else 0
-    recon = eman2_utility.backproject_nn4(gen1, align1, npad=npad, sym=sym, weighting=weighting)
-    mpi_utility.block_reduce(recon[1].ravel(), root=root, **extra)
-    mpi_utility.block_reduce(recon[2].ravel(), root=root, **extra)
-    if root != 0:
-        mpi_utility.send_to_root(recon[1].ravel(), root, **extra)
-        mpi_utility.send_to_root(recon[2].ravel(), root, **extra)
-    recon1 = recon if mpi_utility.get_rank(**extra)==root else None
-    
-    root = 2 if mpi_utility.get_size(**extra) > 2 else 0
-    recon = eman2_utility.backproject_nn4(gen2, align2, npad=npad, sym=sym, weighting=weighting)
-    
-    mpi_utility.block_reduce(recon[1].ravel(), root=root, **extra)
-    mpi_utility.block_reduce(recon[2].ravel(), root=root, **extra)
-    if root != 0:
-        mpi_utility.block_reduce_root(recon[1].ravel(), root, **extra)
-        mpi_utility.block_reduce_root(recon[2].ravel(), root, **extra)
-        if mpi_utility.is_root(**extra) or mpi_utility.get_rank(**extra)==root:
-            return eman2_utility.finalize_nn4(recon)
-        elif recon1 is not None:
-            return eman2_utility.finalize_nn4(recon1)
-        return None
-    else:
-        vol = eman2_utility.finalize_nn4(recon1, recon, npad=npad, sym=sym, weighting=weighting)
-        vol1 = eman2_utility.finalize_nn4(recon1)
-        vol2 = eman2_utility.finalize_nn4(recon)
-        return (vol, vol1, vol2)
 
 def reconstruct_fft(backproject, gen, image_size, align, npad=2, **extra):
     '''Reconstruct a single volume with the given image generator and alignment file.
