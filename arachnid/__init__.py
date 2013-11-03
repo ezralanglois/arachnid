b18839ee0d777d8ace337a5741a9dbb20e5d6d67
Robert Langlois <ezralanglois@gmail.com>
2013-11-03 09:45:52 -0500

    Added global url for docs and updated keywords

--- arachnid/__init__.py
+++ arachnid/__init__.py
@@ -53,16 +53,17 @@ __license__ = "GPL"
 __author_email__ = "rl2528@columbia.edu"
 __description__ = "Single Particle Data Analysis Suite"
 __url__ = "http://code.google.com/p/arachnid/"
+__doc_url__ = "http://code.google.com/p/arachnid/docs/api_generated/%s.html"
 #__download_url__  = "http://www.columbia.edu/cu/franklab/autopart.zip"
-__keywords__ = "cryo-EM particle-picking image-processing"
+__keywords__ = "cryo-EM particle picking image-processing single-particle reconstruction machine learning"
 __platforms__ = "linux"
 __classifiers__ = [
     'Development Status :: 4 - Beta',
     'Environment :: Console',
     'Environment :: MacOS X',
-    'Environment :: Win32 (MS Windows)',
-    #'Environment :: X11 Applications',
-    #'Intended Audience :: End Users/Desktop',
+    #'Environment :: Win32 (MS Windows)',
+    'Environment :: X11 Applications',
+    'Intended Audience :: End Users/Desktop',
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: GNU General Public License (GPL)',
@@ -73,6 +74,8 @@ __classifiers__ = [
     'Topic :: Scientific/Engineering :: Image Recognition',
     'Topic :: Scientific/Engineering :: Bio-Informatics',
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Cryo-electron Microscopy',
+    'Topic :: Scientific/Engineering :: Single-particle reconstruction',
     #'Operating System :: Microsoft :: Windows',
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: Unix',
