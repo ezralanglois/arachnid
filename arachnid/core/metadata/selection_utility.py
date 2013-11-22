27c0b91ef0b255cc51b0b5c2690b8bf2691a5b2f
Robert Langlois <ezralanglois@gmail.com>
2013-11-22 13:27:05 -0500

    Moved functions from format to selection utility

--- arachnid/core/metadata/selection_utility.py
+++ arachnid/core/metadata/selection_utility.py
@@ -6,7 +6,8 @@ This module provides a set of functions to handle selections.
 .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
 '''
 
-import spider_utility
+import spider_utility, format_utility
+import numpy
 
 def select_file_subset(files, select, id_len=0, fill=False):
     ''' Create a list of files based on the given selection
@@ -70,5 +71,82 @@ def select_subset(vals, select):
     else:
         return [vals[s[0]-1] for s in select]
 
+def create_selection_doc(n, start=1, micrograph_id=None):
+    ''' Create a selection document from a range and optional micrograph id
+    
+    The default header for the output namedtuple is id,select. If micrograph_id is
+    specified, then new header becomes micrograph,particle.
+    
+    >>> from arachnid.core.metadata.selection_utility import *
+    >>> create_selection_doc(3)
+    [Selection(id=1, select=1),Selection(id=2, select=1),Selection(id=3, select=1)]
+    
+    >>> create_selection_doc(3,1,10)
+    [Selection(micrograph=10, particle=1),Selection(micrograph=10, particle=2),Selection(micrograph=10, particle=3)]
+    
+    :Parameters:
+    
+    n : int
+        Length of the range
+    start : int
+            Starting value for the range, if 0, then all the values are incremented
+            by 1.
+    micrograph_id : int, optional
+                    Micrograph id
+                    
+    :Returns:
+    
+    vals : array
+           List of namedtuples
+    '''
+    
+    values = numpy.ones((n, 2))
+    pid = 0
+    header="id,select"
+    if micrograph_id is not None:
+        pid = 1
+        values[:, 0] = micrograph_id
+        header = "micrograph,particle"
+    values[:, pid] = range(start, start+n)
+    if start == 0:  values[:, pid] += 1
+    return format_utility.create_namedtuple_list(values, "Selection", header=header)
+
+def create_selection_map(offset, n, micrograph_id):
+    ''' Create a selection document that maps a global ID
+    to micrograph, stack slice IDs.
+    
+    >>> from arachnid.core.metadata.selection_utility import *
+    >>> create_selection_map(50, 2, 10)
+    [Selection(id=50, micrograph=10,slice_id=1),Selection(id=51, micrograph=10,slice_id=2)]
+    
+    >>> create_selection_map(50, [3,9], 10)
+    [Selection(id=50, micrograph=10,slice_id=3),Selection(id=51, micrograph=10,slice_id=9)]
+
+    :Parameters:
+    
+    offset : int
+             Offset for global range of ids
+    n : int or array
+        List of ids or length (1...n+1)
+    micrograph_id : int
+                    Micrograph id
+                    
+    :Returns:
+    
+    vals : array
+           List of namedtuples
+    '''
+    
+    if hasattr(n, '__iter__'):
+        ids = numpy.asarray(n)
+        n = len(ids)
+    else:
+        ids = numpy.arange(1, 1+n, dtype=numpy.int)
+    values = numpy.ones((n, 3), dtype=numpy.int)
+    values[:, 0] = numpy.arange(offset+1, offset+1+n, dtype=numpy.int)
+    values[:, 1] = micrograph_id
+    values[:, 2] = ids
+    header = "id,micrograph,slice_id"
+    return format_utility.create_namedtuple_list(values, "Selection", header=header)
 
 
