b4d9adcf6e05608e6f758c1e1d2905e380f81607
Robert Langlois <ezralanglois@gmail.com>
2013-11-25 14:25:38 -0500

    Added micrograph screen and pow

--- arachnid/util/project.py
+++ arachnid/util/project.py
@@ -293,12 +293,14 @@ def setup_options(parser, pgroup=None, main_option=False):
     shrgroup.add_option("", coordinate_file="data/local/coords/sndc_000000.dat",        help="Location for particle coordinates on micrograph", gui=dict(filetype="open"))
     shrgroup.add_option("", particle_file="data/cluster/win/win_000000.dat",            help="Location for windowed particle stacks", gui=dict(filetype="open"))
     shrgroup.add_option("", ctf_file="data/local/ctf/ctf.dat",                          help="Location of estimated CTF parameters per micrograph", gui=dict(filetype="open"))
+    shrgroup.add_option("", pow_file="data/local/ctf/pow/pow.dat",                      help="Location of power spectra for each micrograph", gui=dict(filetype="open"))
     shrgroup.add_option("", reference_file="data/cluster/reference.dat",                help="Location of generated reference", gui=dict(filetype="open"))
     shrgroup.add_option("", align_file="data/cluster/data/data.star",                   help="Location of relion selection file", gui=dict(filetype="open"))
     shrgroup.add_option("", good_file="",                                               help="Location of cleaned up particle selection files", gui=dict(filetype="open"))
     #data/local/vicer/good/good_000000.dat
     shrgroup.add_option("", view_file="data/local/vicer/view/view_000000.dat",          help="Location of images embedded in low-dimensional factor space", gui=dict(filetype="open"))
     shrgroup.add_option("", selection_file="data/local/screen/select.dat",              help="Location of micrograph selection", gui=dict(filetype="open"))
+    shrgroup.add_option("", small_micrograph_file="data/local/mic_sm/mic_000000.dat",   help="Location of micrograph selection", gui=dict(filetype="open"))
     shrgroup.add_option("", frame_shift_file="data/local/movie/shift/shift_000000.dat", help="Location of frame shifts for each micrograph", gui=dict(filetype="open"))
     pgroup.add_option_group(shrgroup)
     # create suffix system? internal database?
