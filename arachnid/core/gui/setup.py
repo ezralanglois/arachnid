d68878195b3b34f0adafe4c3cae2f08c7f497a9e
Robert Langlois <ezralanglois@gmail.com>
2013-11-28 19:19:17 -0500

    Removed old GUI

--- arachnid/core/gui/setup.py
+++
@@ -1,9 +0,0 @@
-''' Setup for gui scripts
-'''
-
-gui_scripts = [
- 'plot = arachnid.core.gui.embed_viewer:main',
- 'view = arachnid.core.gui.image_viewer:main',
- 'train = arachnid.core.gui.image_viewer:main_train',
- 'projectw = arachnid.core.gui.project_wizard:main',
-]
