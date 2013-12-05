d57dcec0bff4043cf2000b78042c0e549a3c7410
Robert Langlois <ezralanglois@gmail.com>
2013-12-05 14:39:20 -0500

    Most session attributes correct - need to connect back to project

--- arachnid/core/gui/LeginonUI.py
+++ arachnid/core/gui/LeginonUI.py
@@ -5,9 +5,12 @@
 
 from pyui.LeginonUI import Ui_Form
 from util.qt4_loader import QtGui,QtCore,qtSlot
+from model.ListTableModel import ListTableModel
 from ..metadata import leginondb
 import logging
-import sys, os
+
+print "Handelers", len(logging.getLogger('sqlalchemy').handlers)
+print "Handelers2", len(logging.getLogger('sqlalchemy.engine').handlers)
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
@@ -64,12 +67,23 @@ class Widget(QtGui.QWidget):
         projectDB = self.ui.projectDBLineEdit.text()
         alternteUser = self.ui.alternateUserLineEdit.text()
         try:
-            sessions = leginondb.projects_for_user(username, password, leginonDB, projectDB, alternteUser)
+            user = leginondb.projects_for_user(username, password, leginonDB, projectDB, alternteUser)
         except:
             _logger.exception("Error accessing project")
             self.ui.loginStackedWidget.setCurrentIndex(1)
         else:
-            self.ui.projectTableView.setModel(LeginonDBModel(sessions))
+            sessions = user.projects[0].sessions
+            header=['Session', 'Project', 'Images', 'Voltage', 'Pixel Size', 'Magnification', 'CS']
+            data = []
+            for session in sessions[:3]:
+                project=session.projects[0]
+                if len(session.exposures) == 0: continue
+                voltage = session.scope.voltage/1000
+                cs = session.scope.instrument.cs*1e3
+                magnification = session.exposures[0].scope.magnification
+                pixel_size = session.exposures[0].pixelsize*1e10
+                data.append( (session.name, project.name, len(session.exposures), voltage, pixel_size, magnification, cs))
+            self.ui.projectTableView.setModel(ListTableModel(data, header))
     
     def showEvent(self, evt):
         '''Window close event triggered - save project and global settings 
@@ -129,103 +143,3 @@ class Widget(QtGui.QWidget):
         settings.setValue('alternate-user', self.ui.alternateUserLineEdit.text())
         settings.endGroup()
 
-class LeginonDBModel(QtCore.QAbstractTableModel):#QAbstractItemModel):
-    '''
-    '''
-
-    def __init__(self, sessions, parent=None):
-        '''
-        '''
-        
-        QtCore.QAbstractTableModel.__init__(self, parent)
-        self._data = []
-        self._header=['Session', 'Project', 'Images', 'Voltage', 'Pixel Size', 'Magnification', 'CS']
-        
-        for session in sessions[:3]:
-            project=session.projects[0]
-            if len(session.imagedata) == 0: continue
-            #apix = res*(10.0**4.0)/xmag - microns
-            voltage = session.scope.voltage/1000
-            cs = session.scope.instrument.cs*1e3
-            pixel_size = session.camera.pixel_size
-            
-            img = leginondb.find_exposures(session).all()
-            
-            magnification = img[0].scope.magnification
-            self._data.append( (session.name, project.name, len(img), voltage, pixel_size, magnification, cs))
-    
-    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-        '''Returns the data for the given role and section in the header with the specified orientation
-        
-        :Parameters:
-        
-        section : QModelIndex
-                  Index of a model item
-        orientation : QModelIndex
-                      Index of a model item
-        role : QModelIndex
-               Role of data to return
-        
-        :Returns:
-        
-        val : ItemFlags
-              Flags for item at index
-        '''
-        
-        if (orientation, role) == (QtCore.Qt.Horizontal, QtCore.Qt.DisplayRole):
-            return self._header[section]
-        return None
-    
-    def data(self, index, role = QtCore.Qt.DisplayRole):
-        ''' Returns the data stored under the given role for the item referred to by the index
-        
-        :Parameters:
-        
-        index : QModelIndex
-                Index of a model item
-        role : enum
-               Data role
-        
-        :Returns:
-        
-        val : object
-              Data object
-        '''
-        
-        if not index.isValid(): return None
-        if role == QtCore.Qt.DisplayRole:
-            return str(self._data[index.row()][index.column()])
-        return None
-    
-    def rowCount(self, parent = QtCore.QModelIndex()):
-        ''' Get the number of rows for the parent item
-        
-        :Parameters:
-    
-        parent : QModelIndex
-                 Parent of item
-        
-        :Returns:
-        
-        val : int
-              Number of rows
-        '''
-        
-        return len(self._data) if self._data is not None else 0
-    
-    def columnCount(self, parent = QtCore.QModelIndex()):
-        ''' Get the number of columns - 2
-        
-        :Parameters:
-    
-        parent : QModelIndex
-                 Parent of item
-        
-        :Returns:
-        
-        val : int
-              Return value: 2
-        '''
-        
-        return len(self._data[0]) if self._data is not None and len(self._data) > 0 else 0
-    
--- arachnid/core/metadata/leginondb.py
+++ arachnid/core/metadata/leginondb.py
@@ -11,28 +11,47 @@ necessary to process the data.
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker #scoped_session
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, column_property
 from sqlalchemy import Table, Column, Integer, String, ForeignKey, DateTime, desc, Float
+from sqlalchemy import select, and_
 import sqlalchemy
 import logging
 import sys
 
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+
+_sa_logger=logging.getLogger('sqlalchemy')
+while len(_sa_logger.handlers) > 0: _sa_logger.removeHandler(_sa_logger.handlers[0])
+_sa_logger.setLevel(logging.ERROR)
+
 Base = declarative_base()
 
-project_user_table = Table('projectowners', Base.metadata,
-    Column('REF|leginondata|UserData|user', Integer, ForeignKey('UserData.DEF_id')),
-    Column('REF|projects|project', Integer, ForeignKey('projects.DEF_id'))
-)
-project_user_table.__bind_key__='projectdb'
 
-if 1 == 1:
+
+if 1 == 0:
     project_session_table = Table('projectexperiments', Base.metadata,
         Column('REF|projects|project', Integer, ForeignKey('projects.DEF_id')),
         Column('REF|leginondata|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
     )
     project_session_table.__bind_key__='projectdb'
-
+    
+    project_user_table = Table('projectowners', Base.metadata,
+        Column('REF|leginondata|UserData|user', Integer, ForeignKey('UserData.DEF_id')),
+        Column('REF|projects|project', Integer, ForeignKey('projects.DEF_id'))
+    )
+    project_user_table.__bind_key__='projectdb'
 else:
+    
+    class ProjectOwners(Base):
+        '''
+        '''
+        __bind_key__ = 'projectdb'
+        __tablename__='projectowners'
+        
+        id = Column('DEF_id', Integer, primary_key=True)
+        user_id = Column('REF|leginondata|UserData|user', Integer, ForeignKey('UserData.DEF_id'))
+        project_id = Column('REF|projects|project', Integer, ForeignKey('projects.DEF_id'))
     
     class ProjectExperiments(Base):
         '''
@@ -42,7 +61,7 @@ else:
         
         id = Column('DEF_id', Integer, primary_key=True)
         project_id = Column('REF|projects|project', Integer, ForeignKey('projects.DEF_id'))
-        session_id = Column('REF|leginondata|SessionData|session', Integer)
+        session_id = Column('REF|leginondata|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
 
 class User(Base):
     '''
@@ -52,8 +71,9 @@ class User(Base):
     
     username = Column('username', String)
     id = Column('DEF_id', Integer, primary_key=True)
-    projects = relationship("Projects", secondary=project_user_table)
+    projects = relationship("Projects", secondary=ProjectOwners.__table__)#project_user_table)
     sessions = relationship("Session", order_by=lambda: desc(Session.timestamp))
+    #allsessions = relationship("Session", primaryjoin="and_(User.id==ProjectOwners.user_id, ProjectOwners.project_id==ProjectExperiments.project_id, Session.id==ProjectExperiments.session_id)", secondary=ProjectOwners.__table__, order_by=lambda: desc(Session.timestamp))
 
 class Projects(Base):
     '''
@@ -65,7 +85,7 @@ class Projects(Base):
     name = Column('name', String)
     timestamp = Column('DEF_timestamp', DateTime)
     #sessions = relationship("ProjectExperiments")  # - Workaround
-    #sessions = relationship("Session", secondary=project_session_table) - Bug? assumes secondary is on other database
+    sessions = relationship("Session", secondary=ProjectExperiments.__table__) # - Bug? assumes secondary is on other database
     
 class Session(Base):
     __bind_key__ = 'leginondb'
@@ -74,15 +94,50 @@ class Session(Base):
     name = Column('name', String)
     id = Column('DEF_id', Integer, primary_key=True)
     imagedata = relationship("ImageData")
+    exposures = relationship("ImageData", primaryjoin="and_(Session.id==ImageData.session, ImageData.label.startswith('Exposure'))")
     imagefilter = relationship("ImageData", lazy="dynamic")
     timestamp = Column('DEF_timestamp', DateTime)
     user = Column('REF|UserData|user', Integer, ForeignKey('UserData.DEF_id'))
-    projects = relationship("Projects", secondary=project_session_table)
+    projects = relationship("Projects", secondary=ProjectExperiments.__table__)#project_session_table)
     instrument_id = Column('REF|InstrumentData|instrument', Integer, ForeignKey('InstrumentData.DEF_id'))
     instrument = relationship("Instrument", uselist=False)
-    calibration = relationship("PixelSizeCalibration", uselist=False)
     scope = relationship("ScopeEM", uselist=False)
     camera = relationship("CameraEM", uselist=False)
+
+class ScopeEM(Base):
+    __bind_key__ = 'leginondb'
+    __tablename__='ScopeEMData'
+    
+    id = Column('DEF_id', Integer, primary_key=True)
+    voltage = Column('high tension', Float)
+    magnification = Column('magnification', Integer, ForeignKey('PixelSizeCalibrationData.magnification'))
+    session_id = Column('REF|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
+    instrument_id = Column('REF|InstrumentData|tem', Integer, ForeignKey('InstrumentData.DEF_id'))
+    instrument = relationship("Instrument", uselist=False)
+
+class PixelSizeCalibration(Base):
+    __bind_key__ = 'leginondb'
+    __tablename__='PixelSizeCalibrationData'
+    
+    id = Column('DEF_id', Integer, primary_key=True)
+    pixelsize = Column('pixelsize', Float)
+    magnification = Column('magnification', Integer)
+    session_id=Column('REF|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
+    camera_id=Column('REF|InstrumentData|ccdcamera', Integer, ForeignKey('CameraEMData.REF|InstrumentData|ccdcamera'))
+    instrument_id=Column('REF|InstrumentData|tem', Integer)#, ForeignKey('ImageData.REF|ScopeEMData|scope'))
+    #session = relationship("PixelSizeCalibration", uselist=False, backref='calibration')
+
+
+class CameraEM(Base):
+    __bind_key__ = 'leginondb'
+    __tablename__='CameraEMData'
+    
+    id = Column('DEF_id', Integer, primary_key=True)
+    pixel_size = Column('SUBD|pixel size|x', Float)
+    pixel_sizey = Column('SUBD|pixel size|y', Float)
+    session_id = Column('REF|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
+    instrument_id = Column('REF|InstrumentData|ccdcamera', Integer, ForeignKey('PixelSizeCalibrationData.REF|InstrumentData|ccdcamera'))
+    #calibration = relationship("PixelSizeCalibration", uselist=False)
 
 class ImageData(Base):
     __bind_key__ = 'leginondb'
@@ -99,34 +154,15 @@ class ImageData(Base):
     scope = relationship("ScopeEM", uselist=False)
     camera = relationship("CameraEM", uselist=False)
     #pixeltype = Column('pixeltype', Float) - numpy.dtype
+    magnification = column_property(select([ScopeEM.magnification]).where(scope_id==ScopeEM.id))
+    pixelsize = column_property(select([PixelSizeCalibration.pixelsize]).where(
+                                                                               and_(ScopeEM.magnification==PixelSizeCalibration.magnification, 
+                                                                                    scope_id==ScopeEM.id,
+                                                                                    camera_id==CameraEM.id,
+                                                                                    CameraEM.instrument_id==PixelSizeCalibration.camera_id,  #All
+                                                                                    ScopeEM.instrument_id==PixelSizeCalibration.instrument_id)).limit(1))
+    #calibration = relationship('PixelSizeCalibration', secondary=ScopeEM.__table__, uselist=True)
 
-class ScopeEM(Base):
-    __bind_key__ = 'leginondb'
-    __tablename__='ScopeEMData'
-    
-    id = Column('DEF_id', Integer, primary_key=True)
-    voltage = Column('high tension', Float)
-    magnification = Column('magnification', Float)
-    session_id = Column('REF|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
-    instrument_id = Column('REF|InstrumentData|tem', Integer, ForeignKey('InstrumentData.DEF_id'))
-    instrument = relationship("Instrument", uselist=False)
-
-class CameraEM(Base):
-    __bind_key__ = 'leginondb'
-    __tablename__='CameraEMData'
-    
-    id = Column('DEF_id', Integer, primary_key=True)
-    pixel_size = Column('SUBD|pixel size|x', Float)
-    pixel_sizey = Column('SUBD|pixel size|y', Float)
-    session_id = Column('REF|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
-    
-class PixelSizeCalibration(Base):
-    __bind_key__ = 'leginondb'
-    __tablename__='PixelSizeCalibrationData'
-    id = Column('DEF_id', Integer, primary_key=True)
-    pixelsize = Column('pixelsize', Float)
-    session_id=Column('REF|SessionData|session', Integer, ForeignKey('SessionData.DEF_id'))
-    #session = relationship("PixelSizeCalibration", uselist=False, backref='calibration')
     
 class Instrument(Base):
     __bind_key__ = 'leginondb'
@@ -135,6 +171,7 @@ class Instrument(Base):
     id = Column('DEF_id', Integer, primary_key=True)
     cs = Column('cs', Float)
     pixel_size = Column('camera pixel size', Float)
+    #calibration = relationship("PixelSizeCalibration", uselist=False)
 
 def find_exposures(session):
     '''
@@ -147,15 +184,15 @@ def projects_for_user(username, password, leginondb='leginondb', projectdb='proj
     '''
     
     leginondb;projectdb;password; # pyflakes hack
-    leginondb = sqlalchemy.create_engine('mysql://{username}:{password}@{leginondb}'.format(**locals()))
-    projectdb = sqlalchemy.create_engine('mysql://{username}:{password}@{projectdb}'.format(**locals()))
+    leginondb = sqlalchemy.create_engine('mysql://{username}:{password}@{leginondb}'.format(**locals()), echo=False, echo_pool=False)
+    projectdb = sqlalchemy.create_engine('mysql://{username}:{password}@{projectdb}'.format(**locals()), echo=False, echo_pool=False)
     local_vars = locals()
     binds = dict([(v, local_vars[getattr(v, '__bind_key__')])for v in sys.modules[__name__].__dict__.values() if hasattr(v, '__bind_key__')])
     SessionDB = sessionmaker(autocommit=False,autoflush=False)
     db_session = SessionDB(binds=binds)
     if not alternate_user: alternate_user = username
     rs = db_session.query(User).filter(User.username==alternate_user).all()
-    if len(rs) > 0: return rs[0].sessions
+    if len(rs) > 0: return rs[0]
     return []
 
 
