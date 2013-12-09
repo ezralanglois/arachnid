ffd16c9b0e13fc2da215e95be27ff2f8eb8d1581
Robert Langlois <ezralanglois@gmail.com>
2013-12-09 10:51:29 -0500

    Added new logos to icons

---
+++ arachnid/core/gui/icons/icons.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/Users/robertlanglois/workspace/arachnida/src/arachnid/core/gui/icons/icons.qrc'
+#
+# Created: Sun Dec  8 14:12:01 2013
+#      by: pyside-uic 0.2.13 running on PySide 1.1.0
+#
+# WARNING! All changes made in this file will be lost!
+
