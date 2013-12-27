ade4aed9fe9f0ed38c945d65e7db85b5911a81e8
Robert Langlois <ezralanglois@gmail.com>
2013-12-27 08:15:56 -0500

    Added frame alignment

---
+++ arachnid/app/align_frames.py
@@ -0,0 +1,431 @@
+''' Micrograph alignment for tilt, defocus, or dose fractionated series
+
+.. Created on Dec 19, 2013
+.. codeauthor:: Robert Langlois <rl2528@columbia.edu>
+.. codeauthor:: Ryan Hyde Smith <rhs2132@columbia.edu>
+.. codeauthor:: Hstau Liao <hl2485@columbia.edu>
+'''
+from ..core.app import program
+from ..core.image import ndimage_utility
+from ..core.image import ndimage_file
+from ..core.image import ndimage_interpolate
+from ..core.image import ndimage_filter
+#from ..core.image import similarity
+from ..core.metadata import format
+from ..core.metadata import spider_params
+from ..core.metadata import spider_utility
+import scipy.fftpack
+import numpy
+import logging
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+
+
+def process(filename, **extra):
+    '''Concatenate files and write to a single output file
+        
+    :Parameters:
+    
+    filename : str
+               Input filename
+    extra : dict
+            Unused key word arguments
+                
+    :Returns:
+    
+    filename : str
+               Current filename
+    coords : str
+             Coordinates
+    '''
+    
+    spider_utility.update_spider_files(extra, filename, *extra['outfile_deps'])
+    coords = align_in_memory(filename, **extra)
+    if len(coords) > 0:
+        write_average(filename, coords, **extra)
+        write_coordinates(coords, **extra)
+    return filename, coords
+
+def align_in_memory(filename, gain_file="", bin_factor=1.0, lowpass_sigma=None, **extra):
+    '''
+    .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
+    .. codeauthor:: Ryan Hyde Smith <rhs2132@columbia.edu>
+    '''
+    
+    gain = ndimage_file.read_image(gain_file) if gain_file != "" else None
+    fourier_frames = []
+    ssds=[]
+    avg = None
+    ideal_pow=None
+    for frame in ndimage_file.iter_images(filename):
+        frame = frame.astype(numpy.float)
+        if gain is not None: numpy.multiply(frame, gain, frame)
+        if bin_factor > 1.0: frame=ndimage_interpolate.downsample(frame, bin_factor)
+        ndimage_utility.normalize_standard(frame, var_one=True, out=frame)
+        ssds.append((frame, numpy.square(frame).sum()))
+        if avg is not None: avg += frame
+        else: avg=frame
+        pow = perdiogram(frame, **extra)
+        if ideal_pow is not None: ideal_pow += pow
+        else: ideal_pow=pow
+        frame = scipy.fftpack.fft2(frame)
+        fourier_frames.append(frame)
+    write_perdiogram(avg, 0, **extra)
+    ndimage_file.write_image(extra['pow_file'], ideal_pow, 1)
+    
+    _logger.info("Begin sequential")
+    trans = align_sequential(fourier_frames, ssds, **extra)
+    avg = average_fft(fourier_frames, trans)
+    pow1 = write_perdiogram(avg, 2, **extra)
+    _logger.info("Sequential error %f"%(numpy.sqrt(numpy.sum(numpy.square(pow1-ideal_pow)))))
+    _logger.info("Begin l2")
+    trans = align_l2(fourier_frames, **extra)
+    avg = average_fft(fourier_frames, trans)
+    pow1=write_perdiogram(avg, 3, **extra)
+    _logger.info("l2 error %f"%(numpy.sqrt(numpy.sum(numpy.square(pow1-ideal_pow)))))
+    
+    _logger.info("Begin vote")
+    trans=align_vote(fourier_frames, **extra)
+    trans=refine(fourier_frames, trans, ideal_pow, **extra)
+    avg = average_fft(fourier_frames, trans)
+    pow1=write_perdiogram(avg, 4, **extra)
+    _logger.info("Vote error %f"%(numpy.sqrt(numpy.sum(numpy.square(pow1-ideal_pow)))))
+    
+    '''
+    _logger.info("Begin triplet")
+    trans = align_triplet(fourier_frames, **extra)
+    avg = average_fft(fourier_frames, trans)
+    write_perdiogram(avg, 2, **extra)
+    '''
+    
+    trans *= bin_factor
+    return []
+
+def refine(fourier_frames, trans, ideal_pow, upsampling=2, search_radius=50, lowpass_sigma=None, **extra):
+    '''
+    '''
+    
+    filter_kernel=None
+    if lowpass_sigma is not None:
+        filter_kernel = scipy.fftpack.ifftshift(ndimage_filter.gaussian_lowpass_kernel(fourier_frames[0].shape, lowpass_sigma, numpy.float))
+    else: filter_kernel=1.0
+    
+    avg = average_fft(fourier_frames, trans, False)
+    for iter in xrange(5):
+        pow1=write_perdiogram(scipy.fftpack.ifft2(avg).real, 3, **extra)
+        _logger.info("Refine(%d) error %f"%(iter+1, numpy.sqrt(numpy.sum(numpy.square(pow1-ideal_pow)))))
+        sum = 0
+        for i in xrange(len(fourier_frames)):
+            avg -= scipy.ndimage.fourier_shift(fourier_frames[i], (-trans[i, 1], -trans[i, 0]), -1, 0)
+            if filter_kernel is not None: ref=numpy.multiply(avg, filter_kernel)
+            else: ref = avg
+            
+            y, x, p1 = xcorr_dft_peak(ref, fourier_frames[i], upsampling, search_radius)
+            #print i+1, trans[i, :], x,y
+            avg += scipy.ndimage.fourier_shift(fourier_frames[i], (-y, -x), -1, 0)
+            sum += numpy.sqrt(numpy.sum(numpy.square((x-trans[i, 0], y-trans[i, 1]))))
+            trans[i, :]=(x,y)
+        _logger.info("Distance=%f"%(sum/len(fourier_frames)))
+    return trans
+
+def align_vote(fourier_frames, upsampling=2, search_radius=50, lowpass_sigma=None, **extra):
+    '''
+    '''
+    
+    filter_kernel=None
+    if lowpass_sigma is not None:
+        filter_kernel = scipy.fftpack.ifftshift(ndimage_filter.gaussian_lowpass_kernel(fourier_frames[0].shape, lowpass_sigma, numpy.float))
+    else: filter_kernel=1.0
+    
+    index = numpy.triu_indices(len(fourier_frames), 1)
+    cache = numpy.zeros((len(fourier_frames), len(fourier_frames), 3))
+    for i, j in zip(*index):
+        y, x, p = xcorr_dft_peak(fourier_frames[i]*filter_kernel, fourier_frames[j], upsampling, search_radius)
+        cache[i, j] = (x,y,p)
+        cache[j, i] = (-x,-y,p)
+    
+    trans = numpy.zeros((len(fourier_frames), 2))
+    idx = numpy.arange(1, len(fourier_frames), dtype=numpy.int)
+    for i in xrange(1, trans.shape[0]):
+        cidx = idx[idx != i]
+        weight = 1.0 #cache[0, cidx, 2]+cache[i, cidx, 2]
+        #weight /= weight.sum()
+        
+        trans[i, :] = numpy.mean(weight*(cache[0, cidx, :2]-cache[i, cidx, :2]), axis=0)
+        print trans[i, :], trans[i]-trans[i-1]
+    return trans
+
+def align_sequential(fourier_frames, ssds, upsampling=2, search_radius=50, lowpass_sigma=None, **extra):
+    '''
+    
+    @author: Robert Langlois
+    '''
+    
+    filter_kernel=None
+    trans = numpy.zeros((len(fourier_frames), 2))
+    ref = fourier_frames[0].copy()
+    if lowpass_sigma is not None:
+        filter_kernel = scipy.fftpack.ifftshift(ndimage_filter.gaussian_lowpass_kernel(ref.shape, lowpass_sigma, numpy.float))
+    for i, frame in enumerate(fourier_frames[1:]):
+        if filter_kernel is not None: numpy.multiply(ref, filter_kernel, ref)
+        y, x, p1 = xcorr_dft_peak(ref, frame, upsampling, search_radius, tshape=ssds[i+1][0], template_ssd=numpy.square(scipy.fftpack.ifft2(ref).real).sum())
+        trans[i+1, :]=(x,y)
+        print i+1, trans[i+1, :], trans[i+1, :]-trans[i, :], p1
+        ref += scipy.ndimage.fourier_shift(frame, (-trans[i+1, 1], -trans[i+1, 0]), -1, 0)
+    return trans
+
+def align_l2(fourier_frames, upsampling=2, search_radius=50, lowpass_sigma=None, gap=5, **extra):
+    '''
+    .. codeauthor:: Robert Langlois <rl2528@columbia.edu>
+    .. codeauthor:: Ryan Hyde Smith <rhs2132@columbia.edu>
+    '''
+    
+    filter_kernel=None
+    if lowpass_sigma is not None:
+        filter_kernel = scipy.fftpack.ifftshift(ndimage_filter.gaussian_lowpass_kernel(fourier_frames[0].shape, lowpass_sigma, numpy.float))
+    else: filter_kernel=1.0
+    
+    pairs = []
+    for i in xrange(len(fourier_frames)-1):
+        for j in xrange(i+gap, len(fourier_frames)):
+            assert(numpy.abs(i-j)>=gap)
+            pairs.append((i,j))
+    A = numpy.zeros((len(pairs), len(fourier_frames)-1))
+    for i, p in enumerate(pairs):
+        A[i, p[0]:p[1]] = 1
+    b = numpy.zeros((len(pairs), 2))
+    for i, p in enumerate(pairs):
+        b[i, 1], b[i, 0] = xcorr_dft_peak(fourier_frames[p[0]]*filter_kernel, fourier_frames[p[1]], upsampling, search_radius)[:2]
+    x0 = numpy.linalg.lstsq(A, b)[0]
+    trans = numpy.zeros((len(fourier_frames), 2))
+    trans[1:] = x0.cumsum(axis=0)
+    for i in xrange(trans.shape[0]):
+        if i > 0:
+            print trans[i], trans[i]-trans[i-1]
+        else:
+            print trans[i]
+    return trans
+    
+def align_l1(fourier_frames, upsampling=2, search_radius=50, lowpass_sigma=None, **extra):
+    '''
+    '''
+    
+    pass
+
+def xcorr_dft_peak(f1, f2, usfac, search_radius, y0=0, x0=0, tshape=None, template_ssd=None):
+    '''
+    '''
+    
+    f3 = numpy.multiply(f1, f2.conj())
+    y, x, p = _xcorr_dft_peak(f3, min(2, usfac), search_radius, y0, x0, tshape, template_ssd)
+    y += y0
+    x += x0
+    if usfac > 2:
+        dy, dx, p = _xcorr_dft_peak(f3, usfac, 1.5, y, x, tshape, template_ssd)
+        y += dy
+        x += dx
+    return numpy.asarray((y, x, p))
+
+def _centered(arr, newsize):
+    # Return the center newsize portion of the array.
+    newsize = numpy.asarray(newsize)
+    currsize = numpy.array(arr.shape)
+    startind = (currsize - newsize) / 2
+    endind = startind + newsize
+    myslice = [slice(startind[k], endind[k]) for k in range(len(endind))]
+    return arr[tuple(myslice)]
+    
+def _xcorr_dft_peak(f3, usfac, search_radius, y0=0, x0=0, tshape=None, template_ssd=None):
+    '''
+    '''
+    
+    '''
+    if template_ssd is not None:
+        image = tshape
+        tshape=image.shape
+        CC = scipy.fftpack.ifft2(f3).real.copy()
+        CC = numpy.ascontiguousarray(_centered(CC, numpy.abs(numpy.array(f1.shape)-numpy.array(f2.shape))+1))
+        similarity.normalize_correlation(CC, image, tshape, template_ssd)
+        peak = CC.max()
+    else: peak=0
+    '''
+    
+    
+    ny, nx = f3.shape
+    noyx = numpy.ceil(search_radius*usfac)
+    dftshift = numpy.fix(numpy.ceil(search_radius*usfac)/2)
+    yoff = dftshift - numpy.rint(usfac*y0)
+    xoff = dftshift - numpy.rint(usfac*x0)
+
+    kerny = numpy.exp((-2j*numpy.pi/(ny*usfac)*(numpy.arange(noyx).T - yoff)[:, numpy.newaxis])*(scipy.fftpack.ifftshift(numpy.arange(ny) - numpy.floor(ny/2)).T[numpy.newaxis, :]))
+    kernx = numpy.exp((-2j*numpy.pi/(nx*usfac)*(numpy.arange(noyx).T - xoff)[:, numpy.newaxis])*(scipy.fftpack.ifftshift(numpy.arange(nx) - numpy.floor(nx/2)).T[numpy.newaxis, :])).T
+
+    CC = numpy.dot(numpy.dot(kerny, f3), kernx)
+    
+    
+    dy, dx = numpy.unravel_index(numpy.argmax(CC), CC.shape)
+    peak=CC[dy,dx].real
+    dy = (float(dy) - dftshift)/usfac
+    dx = (float(dx) - dftshift)/usfac
+    return dy, dx, peak
+
+def average_fft(fourier_frames, trans, do_ifft=True):
+    '''
+    '''
+    
+    avg = numpy.zeros_like(fourier_frames[0])
+    for i, frame in enumerate(fourier_frames):
+        avg += scipy.ndimage.fourier_shift(frame, (-trans[i, 1], -trans[i, 0]), -1, 0)
+    return scipy.fftpack.ifft2(avg).real if do_ifft else avg
+
+def perdiogram(avg, window_size=256, pad=1, overlap=0.5, **extra):
+    '''
+    '''
+    
+    return ndimage_utility.perdiogram(avg, window_size, pad, overlap)
+
+def write_perdiogram(avg, index=0, pow_file="", apix=None, **extra):
+    '''
+    '''
+    
+    if pow_file == "": return
+    pow = perdiogram(avg, **extra)
+    
+    
+    rmin=25.0
+    rmax=apix*2.5
+    
+    min_freq = apix/rmin
+    max_freq = apix/rmax
+    
+    rang = ((max_freq*pow.shape[0])-(min_freq*pow.shape[0]))/2.0
+    freq2 = float(pow.shape[0])/(rang/1)
+    freq1 = float(pow.shape[0])/(rang/15)
+    pow = ndimage_utility.filter_annular_bp(pow, freq1, freq2)
+    
+    ndimage_file.write_image(pow_file, pow, index)
+    return pow
+
+def write_coordinates(coords, translation_file="", **extra):
+    ''' Write the coordinates to a text file
+    '''
+    
+    if translation_file == "": return
+    coords = numpy.hstack((numpy.arange(len(coords))[:, numpy.newaxis], coords))
+    format.write(translation_file, coords, header='id,x,y'.split(','))
+
+def write_average(filename, coords, output, frame_beg=0, frame_end=0, gain_file="", **extra):
+    ''' Average the frames in the stack using the given 
+    translation coordinates.
+    '''
+    
+    gain = ndimage_file.read_image(gain_file) if gain_file != "" else None
+    #frame_beg=0, frame_end=0
+    if gain is not None:
+        avg = gain.copy()
+        avg[:]=0
+    else: avg = None
+    for i, frame in enumerate(ndimage_file.iter_images(filename)):
+        frame = frame.astype(numpy.float)
+        if gain is not None: frame *= gain
+        frame = ndimage_utility.fourier_shift(frame, -coords[i, 0], -coords[i, 1])
+        if avg is None: avg = frame
+        else: avg += frame
+    avg /= len(coords)
+    ndimage_file.write_image(output, avg)
+
+def initialize(files, param):
+    # Initialize global parameters for the script
+    
+    spider_params.read(param['param_file'], param)
+    if param['gain_file']=="":
+        _logger.warn("No gain reference!")
+    else:
+        _logger.info("Gain reference: %s"%param['gain_file'])
+    if param['translation_file']=="":
+        _logger.warn("No translation coordinate file given!")
+    else:
+        _logger.info("Output translation coordinate file: %s"%param['translation_file'])
+    _logger.info("Decimation factor %f"%param['bin_factor'])
+    if param['resolution'] > 0.0:
+        _logger.info("Filter frames to %f (%f)"%(param['resolution'], param['apix']/param['resolution']))
+        param['lowpass_sigma']=param['apix']/param['resolution']
+    
+def reduce_all(val, **extra):
+    # Process each input file in the main thread (for multi-threaded code)
+    
+    filename, coords = val
+    coords;
+    return filename
+
+def finalize(files, **extra):
+    # Finalize global parameters for the script
+    
+    _logger.info("Completed")
+
+def supports(files, **extra):
+    ''' Test if this module is required in the project workflow
+    
+    :Parameters:
+    
+    files : list
+            List of filenames to test
+    extra : dict
+            Unused keyword arguments
+    
+    :Returns:
+    
+    flag : bool
+           True if this module should be added to the workflow
+    '''
+    
+    if len(files) > 0 and ndimage_file.count_images(files[0]) > 1:
+        return True
+    return False
+
+def setup_options(parser, pgroup=None, main_option=False):
+    # Collection of options necessary to use functions in this script
+    
+    from ..core.app.settings import OptionGroup
+    group = OptionGroup(parser, "Align Frames", "Options to control frame alignment",  id=__name__)
+    group.add_option("", frame_beg=0,  help="Index of the first frame to average, 0 means first frame, 1 second and so on", gui=dict(maximum=10000, minimum=0, singleStep=1))
+    group.add_option("", frame_end=0,  help="Index of last frame to average, if 0 use last in stack", gui=dict(maximum=10000, minimum=0, singleStep=1))
+    group.add_option("", gain_file="", help="Gain reference normalization image", gui=dict(filetype="open"))
+    group.add_option("", resolution=0.0, help="Target resolution to lowpass filter frames")
+    group.add_option("", search_radius=50, help="Maximum search radius")
+    group.add_option("", upsampling=2, help="Upsampling factor")
+    group.add_option("", gap=5, help="Gap between pairs for L1/L2 alignment")
+    pgroup.add_option_group(group)
+    if main_option:
+        pgroup.add_option("-i", "--movie-files",          input_files=[],           help="List of filenames for the input micrographs, e.g. mic_*.mrc", required_file=True, gui=dict(filetype="open"), regexp=spider_utility.spider_searchpath)
+        pgroup.add_option("-o", "--micrograph-files",     output="",                help="Output filename for the averaged micrograph image with correct number of digits (e.g. sndc_0000.spi)", gui=dict(filetype="save"), required_file=True)
+        pgroup.add_option("",                             translation_file="",      help="Output filename for the translation coordinates with correct number of digits (e.g. sndc_0000.spi), if empty no coordinates are saved", gui=dict(filetype="save"), required_file=False)
+        pgroup.add_option("",                             pow_file="",              help="Output filename for the power spectra images with correct number of digits (e.g. sndc_0000.spi), if empty no power spectra are saved", gui=dict(filetype="save"), required_file=False)
+        spider_params.setup_options(parser, pgroup, False)
+
+def flags():
+    ''' Get flags the define the supported features
+    
+    :Returns:
+    
+    flags : dict
+            Supported features
+    '''
+    
+    return dict(description = '''Automated movie frame alignment
+                        
+                        Example: Unprocessed film micrograph
+                         
+                        $ ara-align-frames movie_stack.spi -o average_image.spi
+                      ''',
+                supports_MPI=False, 
+                supports_OMP=True,
+                use_version=True)
+
+def main():
+    #Main entry point for this script
+    program.run_hybrid_program(__name__)
+
+if __name__ == "__main__": main()
+
