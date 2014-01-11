c5ad0b725b3bfb125fb7ce93688073ce0d482f99
Robert Langlois <ezralanglois@gmail.com>
2014-01-11 11:25:53 -0500

    Added snippet to classify subtomo and extract param

---
+++ arachnid/snippets/eman2_examples/subtomo/sta_average_selected.py
@@ -0,0 +1,61 @@
+''' Average an aligned stack of subtomograms
+
+Download to edit and run: :download:`sta_average.py <../../arachnid/snippets/sta_average.py>`
+
+To run:
+
+.. sourcecode:: sh
+    
+    $ python sta_average.py
+
+.. note::
+    
+    Requires EMAN2 2.1
+
+.. literalinclude:: ../../arachnid/snippets/sta_average.py
+   :language: python
+   :lines: 20-
+   :linenos:
+'''
+import sys
+
+#from arachnid.core.image.eman2_utility 
+import EMAN2
+#import numpy
+
+if __name__ == '__main__':
+
+    # Parameters
+    subtomo_stack = sys.argv[1]
+    selected = sys.argv[2]
+    output = sys.argv[3]
+    
+    lines = [line.strip() for line in open(selected, 'r')]
+    select = []
+    for line in lines[1:]:
+        b = line.find(',')+1
+        e = line.find(',', b)
+        if e == -1: e = len(line)
+        print b, e
+        select.append(int(line[b:e]))
+    
+    #e.process_inplace(options.normproc[0],options.normproc[1])
+    total=0
+    avgr=EMAN2.Averagers.get('mean.tomo')
+    print "Averaging %d subtomograms"%EMAN2.EMUtil.get_image_count(subtomo_stack)
+    for i in select:
+        e = EMAN2.EMData()
+        e.read_image(subtomo_stack, i)
+        #e*=-1
+        #e.process_inplace('normalize', {})
+        print i+1, e.get_attr('mean')
+        avgr.add_image(e)
+        total += 1
+    print 'Total', total
+    avg=avgr.finish()
+    #mask=EMAN2.EMData(avg["nx"],avg["ny"],avg["nz"])
+    #mask.to_one()
+    #mask.process_inplace('mask.sharp', dict(outer_radius=72))
+    #avg.mult(mask)
+    avg.process_inplace('normalize', {})
+    avg.write_image(output)
---
+++ arachnid/snippets/eman2_examples/subtomo/sta_extract_param.py
@@ -0,0 +1,52 @@
+''' Reextract aligned particles from the tomograph
+
+Download to edit and run: :download:`sta_reextract.py <../../arachnid/snippets/sta_reextract.py>`
+
+To run:
+
+.. sourcecode:: sh
+    
+    $ python sta_reextract.py
+
+.. note::
+    
+    Requires EMAN2 2.1
+
+.. literalinclude:: ../../arachnid/snippets/sta_reextract.py
+   :language: python
+   :lines: 20-
+   :linenos:
+'''
+import sys
+
+#from arachnid.core.image.eman2_utility 
+import EMAN2
+import numpy
+
+if __name__ == '__main__':
+
+    # Parameters
+    alignment = sys.argv[1]
+    output = sys.argv[2]
+    
+    data = []
+    
+    for i in xrange(EMAN2.EMUtil.get_image_count(alignment)):
+        t = EMAN2.EMData()
+        t.read_image(alignment, i)
+        trans = t['xform.align3d']
+        
+        
+        rot = trans.get_rotation('spider')
+        psi, theta, phi = rot['psi'],rot['theta'],rot['phi']
+        tx, ty, tz = trans.get_pre_trans()
+        data.append([psi, theta, phi, tx, ty, tz])
+    numpy.savetxt(output, data, delimiter='\t')
+        
+        
+        # norm, intervt, threshold
+    
+        
+        #ptcl.process_inplace("xform",{"transform":ptcl_parms[0]["xform.align3d"]})
+        
+    
