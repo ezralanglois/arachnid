89279ca68c6ef3de0a35a9000e1bd10ad4f2c27b
Robert Langlois <ezralanglois@gmail.com>
2014-01-13 17:54:58 -0500

    Fixed bug in normalization

--- arachnid/core/image/util/transform.hpp
+++ arachnid/core/image/util/transform.hpp
@@ -4,20 +4,20 @@ typedef long dsize_type;
 //#include <cstdlib>
 
 template<class T>
-T integrate(T* img_int, dsize_type r0, dsize_type c0, dsize_type r1, dsize_type c1)
+T integrate(T* img_int, dsize_type r0, dsize_type c0, dsize_type r1, dsize_type c1, dsize_type icol)
 {
     T sum=0.0;
 
-    sum += img_int[r1, c1];
+    sum += img_int[r1*icol+c1];
 
     if ( (r0 - 1 >= 0) && (c0 - 1 >= 0) )
-        sum += img_int[r0 - 1, c0 - 1];
+        sum += img_int[(r0 - 1)*icol + (c0 - 1) ];
 
     if ( (r0 - 1) >= 0)
-        sum -= img_int[r0 - 1, c1];
+        sum -= img_int[(r0 - 1)*icol + c1];
 
     if ( (c0 - 1) >= 0)
-    	sum -= img_int[r1, c0 - 1];
+    	sum -= img_int[r1*icol + (c0 - 1)];
     return sum;
 }
 
@@ -36,8 +36,8 @@ void normalize_correlation(T* ccmap, dsize_type row, dsize_type col, T* img_int,
 		for(dsize_type c=0;c<col;++c)
 		{
 			dsize_type c_end = c + tcol - 1;
-			T win_sum = integrate(img_int, r, c, r_end, c_end);
-			T win_sum_sq = integrate(img_int_sq, r, c, r_end, c_end);
+			T win_sum = integrate(img_int, r, c, r_end, c_end, icol);
+			T win_sum_sq = integrate(img_int_sq, r, c, r_end, c_end, icol);
 			/*if (r >=irow || r_end >= irow)
 			{
 				fprintf(stderr, "row: %d > %d or %d > %d\n", r, irow, r_end, irow);
@@ -51,16 +51,16 @@ void normalize_correlation(T* ccmap, dsize_type row, dsize_type col, T* img_int,
 			T win_mean_sq = win_sum*win_sum*inv_area;
 			if( win_sum_sq <= win_mean_sq )
 			{
-				ccmap[r,c]=0;
+				ccmap[r*col+c]=0;
 				continue;
 			}
 			T den = std::sqrt((win_sum_sq-win_mean_sq)*ref_ssd);
-			T val = ccmap[r,c];
+			//T val = ccmap[r,c];
 			/*if (r == 0 && c==0)
 			{
 				if(std::isinf(val))fprintf(stderr, "here-inf, %f - %f * %f == (%d,%d) - %f/%f\n", win_sum_sq, win_mean_sq, ref_ssd, r, c, val, den);
 			}*/
-			ccmap[r,c]/=den;
+			ccmap[r*col+c]/=den;
 		}
 	}
 }
