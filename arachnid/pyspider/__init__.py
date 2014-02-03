ff625fa85880219d805ffa3612948c3d7078ba9a
Robert Langlois <ezralanglois@gmail.com>
2014-02-03 14:00:49 -0500

    Removed unused pyspider scripts - updated docs and setup

--- arachnid/pyspider/__init__.py
+++ arachnid/pyspider/__init__.py
@@ -7,18 +7,17 @@
     :toctree: api_generated/
     :template: api_module.rst
     
-    project
-    defocus
-    reference
     align
-    create_align
-    refine
-    reconstruct
+    defocus
     classify
-    prepare_volume
+    create_align
     enhance_volume
     filter_volume
     mask_volume
+    prepare_volume
+    reconstruct
+    refine
     resolution
-    legion_to_spider
+    reference
+    autorefine
 '''
