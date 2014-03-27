60a74aa72ca86af4db9d1335507a001fe3b6b85e
Robert Langlois <ezralanglois@gmail.com>
2014-03-27 10:26:32 -0400

    Added build.sh - conda-build inconsistent when creating the script

---
+++ conda-recipes/daily-mkl/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+base=`dirname $RECIPE_DIR`
+base=`dirname $base`
+cp -r $base .
+cd `basename $base`
+$PYTHON setup.py version
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
